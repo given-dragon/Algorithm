a2fad3606f466436b1887e91b079629f16c4fc8f
given-dragon <yijy001@gmail.com>
2023-12-28 12:41:07 +0900

    solve #45 : 9655 돌 게임-DP 풀이

---
+++ baekjoon/9655/Main_DP.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+// DP 풀이
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N+3];
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 1;
+        for (int i = 4; i <= N; i++) {
+            dp[i] = Math.min(dp[i - 1], dp[i - 3]) + 1;
+        }
+
+        System.out.println(((dp[N] & 1) == 1) ? "SK" : "CY");
+    }
+}
