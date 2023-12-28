6a9537f00d24caf6988782212c06fa626f19b31d
given-dragon <yijy001@gmail.com>
2023-12-28 12:40:05 +0900

    solve #45 : 9655 돌 게임

---
+++ baekjoon/9655/Main.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if ((N & 1) == 1)
+            System.out.println("SK");
+        else
+            System.out.println("CY");
+    }
+}
