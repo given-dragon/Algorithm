b9d024e25b0b0fe8bbf8b4a43e59eec71b9cb3e2
given-dragon <yijy001@gmail.com>
2023-12-29 12:10:00 +0900

    solve #46 : 9095 1, 2, 3 더하기 - dp

---
+++ baekjoon/9095/Main_dp.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int[] dp = new int[n+1+2];
+            sb.append(find(dp, n)).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    private static int find(int[] dp, int n) {
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+        return dp[n];
+    }
+}
