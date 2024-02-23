fde7b39e4010019e18e063cbed5297fbb6896ac0
given-dragon <yijy001@gmail.com>
2024-02-24 00:54:27 +0900

    refactor #98 : 17070 파이프 옮기기

--- baekjoon/17070/Main.java
+++ baekjoon/17070/Main.java
@@ -1,11 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
